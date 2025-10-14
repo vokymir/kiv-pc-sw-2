8cc939fe755281135d161a96bbeefd9877e4203a
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-10-14 20:54:35 +0200

    written TODOs

--- src/lexer.c
+++ src/lexer.c
@@ -1,2 +1,8 @@
 #include "lexer.h"
 #include "memory.h"
+
+// TODO: tokenize/free - look into temp for inspiration
+// TODO: dynamic-length list (for tokens)
+//
+// LATER TODO: linked list (for saved identifiers and their position in
+// data/code segment)
