abc69e2e462b4982235ddae307c3575ebbd9dc4f
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-04 09:39:43 +0100

    update output to be nicer

--- src/output.c
+++ src/output.c
@@ -4,48 +4,53 @@
 #include "fileutil.h"
 #include "output.h"
 
+#define PRINT_VERBOSE(...) print_verbose(asp->config->flag_verbose, __VA_ARGS__)
+
+#define CLEANUP_VERBOSE_IF_FAIL(cond, msg, ...)                                \
+  do {                                                                         \
+    if (!(cond)) {                                                             \
+      PRINT_VERBOSE(msg, ##__VA_ARGS__);                                       \
+      err = ERR_MY_CODE_FAILURE;                                               \
+      goto cleanup;                                                            \
+    }                                                                          \
+  } while (0)
+
 enum Err_Main output_binary(const struct Assembler_Processing *asp) {
   FILE *f = NULL;
+  enum Err_Main err = ERR_NO_ERROR;
+
   if (!asp || !asp->config || !asp->cdsg || !asp->dtsg ||
       !asp->config->target) {
-    print_verbose(
-        asp && asp->config && asp->config->flag_verbose,
+    PRINT_VERBOSE(
         "Tried to write output binary, but some argument is missing.\n");
     return ERR_MY_CODE_FAILURE;
   }
 
-  print_verbose(asp->config->flag_verbose, "Opening a file '%s'.\n",
-                asp->config->target);
+  PRINT_VERBOSE("Opening a file '%s'.\n", asp->config->target);
 
-  if (!fu_open(asp->config->target, &f, "wb")) {
-    print_verbose(asp->config->flag_verbose, "Error opening a file.\n");
+  CLEANUP_VERBOSE_IF_FAIL(fu_open(asp->config->target, &f, "wb"),
+                          "Error opening file '%s'.\n", asp->config->target);
+
+  PRINT_VERBOSE("Writing a KMA header.\n");
+  CLEANUP_VERBOSE_IF_FAIL(fu_write_bytes(f, "KMA", 3),
+                          "Couldn't write KMA at the beginning of the file.\n");
+
+  PRINT_VERBOSE("Writing datasegment.\n");
+  CLEANUP_VERBOSE_IF_FAIL(fu_write_bytes(f, asp->dtsg->bytes, asp->dtsg->size),
+                          "Couldn't write datasegment to the file.\n");
+
+  PRINT_VERBOSE("Writing codesegment.\n");
+  CLEANUP_VERBOSE_IF_FAIL(fu_write_bytes(f, asp->cdsg->bytes, asp->cdsg->size),
+                          "Couldn't write codesegment to the file.\n");
+
+  PRINT_VERBOSE("Writing terminating NULL.\n");
+  CLEANUP_VERBOSE_IF_FAIL(fu_write_bytes(f, "\0", 1),
+                          "Couldn't write NULL terminator.\n");
+
+cleanup:
+  if (f) {
+    PRINT_VERBOSE("Closing file.\n");
+    fclose(f);
   }
-
-  print_verbose(asp->config->flag_verbose, "Writing a KMA header'.\n");
-  if (!fu_write_bytes(f, "KMA", 3)) {
-    print_verbose(asp->config->flag_verbose,
-                  "Couldn't write KMA at the beginning of the file.\n");
-  }
-
-  print_verbose(asp->config->flag_verbose, "Writing datasegment.\n");
-  if (!fu_write_bytes(f, asp->dtsg->bytes, asp->dtsg->size)) {
-    print_verbose(asp->config->flag_verbose,
-                  "Couldn't write datasegment to the file.\n");
-  }
-
-  print_verbose(asp->config->flag_verbose, "Writing codesegment.\n");
-  if (!fu_write_bytes(f, asp->cdsg->bytes, asp->cdsg->size)) {
-    print_verbose(asp->config->flag_verbose,
-                  "Couldn't write codesegment to the file.\n");
-  }
-
-  print_verbose(asp->config->flag_verbose, "Writing terminating NULL.\n");
-  if (!fu_write_bytes(f, "\0", 1)) {
-    print_verbose(asp->config->flag_verbose,
-                  "Couldn't write NULL at the end of the file.\n");
-  }
-
-  print_verbose(asp->config->flag_verbose, "Closing file.\n");
-  fclose(f);
-  return ERR_NO_ERROR;
+  return err;
 }
