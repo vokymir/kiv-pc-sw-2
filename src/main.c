6d9f1f3823a00845da9bb06c1c4628b361f741f6
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-06 19:46:00 +0100

    commented main

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "memory.h"
 #include "output.h"
 
+// If any called function fails, main won't fail, won't leak any memory.
 #define DONT_FAIL(func)                                                        \
   do {                                                                         \
     if ((err = (func)) != ERR_NO_ERROR) {                                      \
@@ -13,35 +14,35 @@
     }                                                                          \
   } while (0)
 
+// Run the transcriber program: With arguments from CLI translate given
+// assembler file into binary file for the virtual KMA computer.
+// Return codes were specified in assignment, and are defined in common.h
 int main(const int argc, const char **argv) {
   struct Config config = {0};
   struct Assembler_Processing *asp = NULL;
   enum Err_Main err = ERR_NO_ERROR;
 
-  // Parse arguments and save results into config.
+  // Parse CLI arguments and save results into config.
   DONT_FAIL(args_parse(&config, argc, argv));
 
-  print_verbose(config.flag_verbose,
-                "Source: %s\nTarget: %s\nVerbose: %s\nInstructions: %s\n",
-                config.source, config.target,
-                config.flag_verbose ? "yes" : "no",
-                config.flag_instruction ? "yes" : "no");
-
+  // Prepare for processing assembler
   asp = asp_create(&config, NULL, NULL, NULL);
   if (!asp) {
     return ERR_MY_CODE_FAILURE;
   }
 
+  // Store the intermediate representation logically separated in asp.
   DONT_FAIL(process_assembler(asp));
 
+  // Assemble the binary and save it to output file.
   DONT_FAIL(output_binary(asp));
 
-// Free all main-related memory, check for leaks and end
+// Free all main-related memory.
 finalize:
   args_config_deinit(&config);
   if (asp) {
     asp_free(&asp);
   }
-  assert(jemory() == 0);
+  assert(jemory() == 0); // Sanity check
   return (int)err;
 }
