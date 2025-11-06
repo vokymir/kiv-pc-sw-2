eeee9e25693cd05049cc4a65e05a7341e90229ea
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-06 09:52:03 +0100

    stderr lexer/token_array.c

--- src/lexer/token_array.c
+++ src/lexer/token_array.c
@@ -5,10 +5,10 @@
 #include "lexer_token_array.h"
 
 int tok_arr_init(struct Token_Arr *arr) {
-  CLEANUP_IF_FAIL(arr, "TODO:");
+  CLEANUP_IF_FAIL_ERR(arr, "Tried to initialize token array from NULL.");
 
   arr->tokens = jalloc(TOKENS_INITIAL_CAPACITY * sizeof(struct Token));
-  CLEANUP_IF_FAIL(arr->tokens, "TODO:");
+  CLEANUP_IF_FAIL_ERR(arr->tokens, "Couldn't allocate token array.");
 
   arr->count = 0;
   arr->capacity = TOKENS_INITIAL_CAPACITY;
@@ -20,7 +20,7 @@ cleanup:
 }
 
 void tok_arr_deinit(struct Token_Arr *arr) {
-  CLEANUP_IF_FAIL(arr, "TODO:");
+  CLEANUP_IF_FAIL_ERR(arr, "Tried to de-initialize token array from NULL.");
 
   if (arr->tokens) {
     jree(arr->tokens);
@@ -36,7 +36,7 @@ cleanup:
 int tok_arr_ensure_capacity(struct Token_Arr *arr, size_t additional_tokens) {
   size_t req = 0, new_cap = 0;
   struct Token *new_tokens = NULL;
-  CLEANUP_IF_FAIL(arr, "TODO:");
+  CLEANUP_IF_FAIL_ERR(arr, "Tried to ensure capacity of token array NULL.");
 
   if (additional_tokens == 0) {
     return 1;
@@ -53,7 +53,7 @@ int tok_arr_ensure_capacity(struct Token_Arr *arr, size_t additional_tokens) {
   }
 
   new_tokens = jealloc(arr->tokens, new_cap * sizeof(struct Token));
-  CLEANUP_IF_FAIL(new_tokens, "TODO:");
+  CLEANUP_IF_FAIL_ERR(new_tokens, "Couldn't grow token array using realloc.");
 
   arr->tokens = new_tokens;
   arr->capacity = new_cap;
