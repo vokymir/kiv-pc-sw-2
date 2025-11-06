29627680dbe1e5fb222779ae3ca471f89fe1390a
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-06 12:31:00 +0100

    fix stderr in args.c

--- src/core/args.c
+++ src/core/args.c
@@ -221,7 +221,8 @@ cleanup:
 
 static const char *_args_find_tgt(const int argc, const char **argv) {
   int i = 0;
-  CLEANUP_IF_FAIL_ERR(argc > 2 && argv, "Invalid arguments were given.");
+  CLEANUP_IF_FAIL_ERR(argv, "Invalid argv was given.");
+  CLEANUP_IF_FAIL_(argc > 2);
 
   for (i = 2; i < argc; i++) { // skip .exe and src argumnets
     if (argv[i][0] != '-') {   // if is not a flag
