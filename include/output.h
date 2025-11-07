bae54c0a6462416b7a47c130fe1cfcfeb1800aed
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-07 18:50:22 +0100

    comment output.h

--- include/output.h
+++ include/output.h
@@ -1,11 +1,15 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
+/* Module for writing the prepared binary to the target file. Uses structures
+ * storing data/code in <asp> and make sure thr outputted binary is valid KMX
+ * file. */
 
 #include "assembler.h"
 #include "common.h"
 
-// Output correct binary from asp to asp->config->target.
-// Ensure correct KMA header, order of segments in file, etc.
+// Output correct binary from <asp> to target file.
+// Ensure KMA header, order of segments in binary file, etc.
+// Return standard error.
 enum Err_Main output_binary(const struct Assembler_Processing *asp);
 
 #endif
