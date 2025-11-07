22e712add4fabfb84ede7a33952e702e0ddcc511
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-07 16:53:35 +0100

    comment fileutils

--- include/fileutil.h
+++ include/fileutil.h
@@ -1,5 +1,6 @@
 #ifndef FILE_UTIL_H
 #define FILE_UTIL_H
+/* Module for working with file, File Utilities. */
 
 #include <stdio.h>
 
@@ -16,8 +17,8 @@ int fu_is_dir(const char *path);
 int fu_can_read(const char *path);
 
 // Return 1 if current process can write into the path.
-// Meaning if path is file, write into file OR in the same directory.
-// If path is dir, if can create files in it.
+// Meaning if path is a file, write into the file if exists OR in the same
+// directory. If path is dir, if can create files in it.
 int fu_can_write(const char *path);
 
 // Return 1 if can write INTO file (path must be file).
@@ -26,8 +27,8 @@ int fu_can_write_file(const char *path);
 // Return 1 if can create files in directory (path must be dir).
 int fu_can_write_dir(const char *path);
 
-// Return 1 if can create files in parent directory, path don't have to be
-// valid.
+// Return 1 if can create files in parent directory. Doesn't check the path
+// validity.
 int fu_can_write_parent_dir(const char *path);
 
 // Open file from path.
@@ -42,7 +43,7 @@ int fu_open(const char *path, FILE **f, const char *modifiers);
 // If *lineptr is NULL or *n is 0, allocate a buffer(caller must free).
 long fu_getline(char **lineptr, size_t *n, FILE *stream);
 
-// Write `count` bytes from `buf` to file.
+// Write <count> bytes from <buf> to file.
 // Returns 1 on success, 0 on failure.
 int fu_write_bytes(FILE *f, const void *buf, size_t count);
 
