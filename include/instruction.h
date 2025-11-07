0e7d0bcc5ce85180cd0ed0bd0f453d1cf8267cab
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-07 18:31:32 +0100

    comment instruction.h

--- include/instruction.h
+++ include/instruction.h
@@ -1,41 +1,48 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
+/* Module for defining all available instructions and a simple interface to work
+ * with them. */
 
 #include <stddef.h>
 #include <stdint.h>
 
-// Operand type for instruction
+// Instruction has at most two operands each of which is of type:
 enum Operand_Type {
-  OP_NONE,
-  OP_REG,
+  OP_NONE,  // The operand is not used
+  OP_REG,   // Register
   OP_IMM32, // immediate 32 bit value
 };
 
+// Descriptor of available instructions - each found instruction in assembly
+// .kas file has to map to some descriptor using its name, mnemonic.
 struct Instruction_Descriptor {
-  const char *mnemonic; // name used in assembly
-  uint8_t opcode;
-  enum Operand_Type operand1;
-  enum Operand_Type operand2;
-  int operand_count; // 0,1,2
+  const char *mnemonic;       // Name used in assembly
+  uint8_t opcode;             // 'Name' used in binary
+  enum Operand_Type operand1; // Type of 1st operand
+  enum Operand_Type operand2; // Type of 2nd operand
+  int operand_count; // How many operands are used. viable values: 0,1,2
 };
 
-// Check if given string is an instruction mnemonic.
-// If len == 0 word must be NULL-terminated, if len is specified,
-// uses it inside strncpy. Return 1 if is instruction, 0 if not.
+// Check if given <word> is an instruction mnemonic.
+// If <len> == 0, <word> must be NULL-terminated, if <len> is specified,
+// only that number of characters will be examined. Return 1 if <word> is
+// instruction, 0 if not.
 int instruction_is_mnemonic(const char *word, const size_t len);
 
-// Find I_D for given mnemonic and types of operands.
-// If len == 0 word must be NULL-terminated, if len is specified,
-// uses it inside strncpy. Return pointer to descriptor or NULL.
+// Find descriptor for given <mnemonic> and types of operands.
+// If <len> == 0 <mnemonic> must be NULL-terminated, if <len> is specified,
+// only that number of characters will be used from <mnemonic>. Return pointer
+// to descriptor or NULL.
 const struct Instruction_Descriptor *instruction_find(const char *mnemonic,
                                                       const size_t len,
                                                       enum Operand_Type op1,
                                                       enum Operand_Type op2);
 
-// Calculate the size of an encoded instruction in bytes,
+// Calculate the size of an encoded instruction <desc>riptor in bytes,
 size_t instruction_get_encoded_size(const struct Instruction_Descriptor *desc);
 
-// If instruction is a jump with relative offset, return 1. Otherwise 0.
+// If instruction <desc>riptor is a jump with relative offset, return 1.
+// Otherwise return 0.
 int instruction_is_relative_jump(const struct Instruction_Descriptor *desc);
 
 #endif
