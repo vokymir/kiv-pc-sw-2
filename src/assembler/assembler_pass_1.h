bcd8c5afb96cfd4ee13109c7838989bc03eb76cb
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-08 17:00:06 +0100

    comment ass/pass1.h

--- src/assembler/assembler_pass_1.h
+++ src/assembler/assembler_pass_1.h
@@ -1,30 +1,40 @@
 #ifndef ASSEMBLER_PASS_1_H
 #define ASSEMBLER_PASS_1_H
+/* Module helper for functionality specific to 1st pass. */
 
 #include <parser.h>
 #include <stdlib.h>
 
 #include "assembler.h"
 
-// Process one line in the first pass of the assembler code.
-// Return adequate error code, edit context if changed.
+// Process one <line> in the first pass of the assembler code.
+// Might edit <context> or any structure inside <asp>, based on what is on the
+// given <line>. Return adequate error code.
 enum Err_Asm pass1_line(struct Assembler_Processing *asp,
                         enum Assembler_Context *ctx, size_t nl,
                         const char *line);
 
-// Decide what to do in pass1 with given pstmt in pass one.
+// Decide which function is called based on information from already parsed line
+// saved inside <pstmt>. Pass the error the called function returned.
 enum Err_Asm pass1_decide(struct Parsed_Statement *pstmt,
                           struct Assembler_Processing *asp,
                           enum Assembler_Context *ctx, size_t nl);
 
+// Process data declaration in .DATA section in the 1st pass. "Reserve" the
+// space for data by advancing the dtsg inside <asp> and store the name to
+// symbol table inside <asp>.
 enum Err_Asm pass1_data_decl(struct Parsed_Statement *pstmt,
                              struct Assembler_Processing *asp,
                              enum Assembler_Context *ctx, size_t nl);
 
+// Process instruction in .CODE section in 1st pass by "reserving" the space for
+// it in the cdsg inside <asp>.
 enum Err_Asm pass1_instruction(struct Parsed_Statement *pstmt,
                                struct Assembler_Processing *asp,
                                enum Assembler_Context *ctx, size_t nl);
 
+// Process label definition in .CODE section in 1st pass by saving it to the
+// symbol table inside <asp>.
 enum Err_Asm pass1_label_def(struct Parsed_Statement *pstmt,
                              struct Assembler_Processing *asp,
                              enum Assembler_Context *ctx, size_t nl);
