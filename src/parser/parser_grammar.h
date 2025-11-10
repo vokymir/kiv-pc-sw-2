3a81ea1a88aa0e7e147463e031eae51e5cb3241e
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-10 11:36:19 +0100

    comment parser/grammar.h

--- src/parser/parser_grammar.h
+++ src/parser/parser_grammar.h
@@ -1,10 +1,11 @@
 #ifndef PARSER_GRAMMAR_H
 #define PARSER_GRAMMAR_H
 
-/* DESCRIPTION OF GRAMMAR
+/* DESCRIPTION OF GRAMMAR - better-aranged in documentation
  * - terminating symbols are in UPPERCASE and have direct equivalent in enum
  * Token_Type
  * - non-terminating in <> and these are named same as their functions
+ * - every possible <line> must be ended by EOF
  * - its ALMOST 3R grammar according to Chomsky:
  *  - on left side can only be one non-terminal
  *  - on right-hand-side can only be:
@@ -13,7 +14,6 @@
  *   ! EOF after TERMINAL could be viewed as violation of 3R grammar !
  *   ! <*dup> are clear violation !
  *   ! <instruction_*> are clear violation !
- * - every possible <line> must be ended by EOF
  *
  * 1) <line> --> <kma_line> | <code_line> | <data_line> | <label_line> |
  * <identifier_line> | <instruction_line> | EOF
@@ -33,9 +33,9 @@
  * NUM, RPAREN, <identifier_dw_dec2> | NUMBER, DUP, LPAREN, QUESTION, RPAREN,
  * <identifier_dw_dec2>
  * 11) <identifier_db_dec> --> QUESTION, <identifier_db_dec2> | NUMBER,
- * <identifier_db_dec2> | STRING, <identifier_db_dec2> | <identifier_db_dup> 12)
- * <identifier_db_dec2> --> COMMA, <identifier_db_dec> | EOF 13)
- * <identifier_db_dup> --> NUMBER, DUP, LPAREN, NUM, RPAREN,
+ * <identifier_db_dec2> | STRING, <identifier_db_dec2> | <identifier_db_dup>
+ * 12) <identifier_db_dec2> --> COMMA, <identifier_db_dec> | EOF
+ * 13) <identifier_db_dup> --> NUMBER, DUP, LPAREN, NUM, RPAREN,
  * <identifier_db_dec2> | NUMBER, DUP, LPAREN, QUESTION, RPAREN,
  * <identifier_db_dec2>
  *
@@ -52,30 +52,35 @@
 #include "lexer.h"
 #include "parser.h"
 
-// TODO: CHECK IF ANY MACRO CAN BE ONLY FILE SCOPED
+// return NO_MATCH if <cond>ition fails
 #define NOMATCH_IF_FAIL(cond) RETURN_IF_FAIL((cond), GRM_NO_MATCH)
+// print stderr end return NO_MATCH if <cond>ition fails
 #define NOMATCH_IF_FAIL_ERR(cond, ...)                                         \
   RET_STDERR_IF_FAIL((cond), GRM_NO_MATCH, __VA_ARGS__)
 
+// make any sequence of Token Types an array
 #define TOK_ARR(...) ((const enum Token_Type[]){__VA_ARGS__})
+// semantic readibility improvement
 #define TOK_CURR tokens[0]
 #define TOK_NEXT tokens[1]
 
-// NEXT
-
+// Error codes used in Grammar module.
+// Original intension was to split generic error into multiple specific errors.
 enum Err_Grm {
-  GRM_MATCH,
-  GRM_NO_MATCH,
-  GRM_GENERIC_ERROR,
+  GRM_MATCH,         // the rest of line matched searched rule
+  GRM_NO_MATCH,      // the rest of line is not this
+  GRM_GENERIC_ERROR, // something bad happened
 };
 
-// TODO: write: only line functions here
+// All below functions are 'full line scoped', meaning if e.g. instruction line
+// is too to parse in one function, other functions are implemented somewhere
+// else. For the purpose of 'high overview' is that not relevant.
 
-// VSECHNY FCE POKUD VOLAJI NEJAKOU DALSI, PREDAJI POINTER UZ POSUNUTEJ NA DALSI
-// TOKEN
+// All functions below if calling another function to parse part of the line,
+// they pass pointer to corresponding token, updateing the position.
 
 // Using grammar, parse the line. Update the pstmt to hold the answer.
-// On success return 1 AND fill the pstmt, so the caller must free.
+// On success return 1 AND fill the pstmt, so the caller must free (deinit).
 // On failure return 0 and allocates nothing.
 enum Err_Grm grammar_line(struct Parsed_Statement *pstmt,
                           const struct Token *tokens[]);
@@ -111,6 +116,9 @@ enum Err_Grm grammar_line_label(struct Parsed_Statement *pstmt,
 enum Err_Grm grammar_line_identifier(struct Parsed_Statement *pstmt,
                                      const struct Token *tokens[]);
 
+// If token array is an instruction, fill the insides of pstmt. On success
+// caller must deinit pstmt & GRM_MATCH is returned. On failure GRM_NO_MATCH is
+// returned with the pstmt unchanged.
 enum Err_Grm grammar_line_instruction(struct Parsed_Statement *pstmt,
                                       const struct Token *tokens[]);
 
